fdbd1d607408b694f00fc91c3a3c0b0d8fcbaf62
hyunjun529 <hyunjun529@gmail.com>
2019-02-04 20:40:10 +0900

    apply bone

--- script/common.js
+++ script/common.js
@@ -1,3 +1,10 @@
+if ( WEBGL.isWebGLAvailable() === false ) {
+    document.body.appendChild( WEBGL.getWebGLErrorMessage() );
+}
+
+var container, stats, controls;
+var camera, scene, renderer, light;
+
 const FACE_MORPH = {
     "EYE_CLOSE" : 12,
     "EYE_CLOSE_R" : 13,
@@ -10,25 +17,57 @@ const FACE_MORPH = {
     "MTH_U" : 31,
     "MTH_E" : 32,
     "MTH_O" : 33,
-}
-
-const BONE = {
-    "standard":null,
-}
+};
 
 const ANIMATION = {
-    "standard":null,
-}
-
-
-if ( WEBGL.isWebGLAvailable() === false ) {
-    document.body.appendChild( WEBGL.getWebGLErrorMessage() );
-}
-
-var container, stats, controls;
-var camera, scene, renderer, light;
+    "Default":null,
+};
 
 var morphMesheFace;
+
+var nodeMeshBody;
+
+var boneBody = {
+    // Chest
+    "J_Bip_C_Chest": -1,
+    // Neck
+    "J_Bip_C_Neck": -1,
+    // Head
+    "J_Bip_C_Head": -1,
+    // Eye, don't use z (1, 1, 0)
+    "J_Adj_L_FaceEye": -1,
+    "J_Adj_R_FaceEye": -1,
+    // UpperArm
+    "J_Bip_L_Shoulder": -1,
+    "J_Bip_R_Shoulder": -1,
+    // LowwerArm
+    "J_Bip_L_LowerArm": -1,
+    "J_Bip_R_LowerArm": -1,
+    // Hand
+    "J_Bip_L_Hand": -1,
+    "J_Bip_R_Hand": -1,
+};
+
+var boneBodyRotatoinLimit = {
+     // Chest
+     "J_Bip_C_Chest": -1,
+     // Neck
+     "J_Bip_C_Neck": -1,
+     // Head
+     "J_Bip_C_Head": -1,
+     // Eye, don't use z (1, 1, 0)
+     "J_Adj_L_FaceEye": -1,
+     "J_Adj_R_FaceEye": -1,
+     // UpperArm
+     "J_Bip_L_Shoulder": -1,
+     "J_Bip_R_Shoulder": -1,
+     // LowwerArm
+     "J_Bip_L_LowerArm": -1,
+     "J_Bip_R_LowerArm": -1,
+     // Hand
+     "J_Bip_L_Hand": -1,
+     "J_Bip_R_Hand": -1,
+}
 
 init();
 
@@ -41,10 +80,13 @@ function init() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xBBBBBB );
 
+    // Lighting
     light = new THREE.HemisphereLight( 0xbbbbff, 0x444422 );
     light.position.set( 0, 10, 0 );
     scene.add( light );
 
+
+    // for Matcap
     var texLoader = new THREE.TextureLoader();
 
     var matcap = texLoader.load( 'resource/textures/matcaps/matcap-porcelain-white.jpg', function () {
@@ -53,19 +95,32 @@ function init() {
 
     var matColor = new THREE.Color(0x444444);
 
+
     var loader = new THREE.GLTFLoader().setPath( 'resource/' );
     loader.load( 'Sendagaya_Shibu.glb', function ( gltf ) {
         gltf.scene.traverse( function ( node ) {
+            // Matcap
             if ( node.isMesh ) {
                 node.material = new THREE.MeshMatcapMaterial( {
                     color: matColor,
                     matcap: matcap,
+                    skinning: true,
                 } );
             }
+
+            if (node.isMesh && node.name == "Body") {
+                nodeMeshBody = node;
+            }
+
             if (node.isMesh && node.morphTargetInfluences) {
-                // this setup is just for MatCap
-                node.material.morphTargets = true;
+                node.material.morphTargets = true; // this setup is just for MatCap
                 morphMesheFace = node;
+            }
+
+            if (node.isBone) {
+                if ( boneBody[node.name] < 0 ) {
+                    boneBody[node.name] = node;
+                }
             }
         } );
         scene.add( gltf.scene );
@@ -73,11 +128,13 @@ function init() {
         console.error( e );
     } );
 
+
     renderer = new THREE.WebGLRenderer( { antialias: true, alpha:true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     onWindowResize();
     renderer.gammaOutput = true;
     container.appendChild( renderer.domElement );
+
 
     controls = new THREE.OrbitControls( camera, renderer.domElement );
     controls.target.set( -0.061435277558714356, 1.3493664567901864, -0.13999196351380783 );
