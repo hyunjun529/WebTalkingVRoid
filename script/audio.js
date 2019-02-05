b5c14f0af55ed6dbc639aec7abc14659be6f1a82
hyunjun529 <hyunjun529@gmail.com>
2019-02-05 22:43:48 +0900

    simple vowel generator

--- script/audio.js
+++ script/audio.js
@@ -1,8 +1,8 @@
 var canvas = document.querySelector('#cvs-audio-visualization-input');
 var canvasCtx = canvas.getContext("2d");
 
- // define audio context
- // Webkit/blink browsers need prefix, Safari won't work without window.
+// define audio context
+// Webkit/blink browsers need prefix, Safari won't work without window.
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
 var gainNode = audioCtx.createGain();
@@ -10,7 +10,7 @@ var analyser = audioCtx.createAnalyser();
 
 function startPoint() {
     // constraints - only audio needed for this app
-    navigator.getUserMedia( { audio: true },
+    navigator.getUserMedia({ audio: true },
 
         // Success callback
         function (stream) {
@@ -29,53 +29,110 @@ function startPoint() {
     );
 }
 
-// 이거를 visualize 할 것
+var samplingDuration = 1;
+var smaplingCnt = 0;
+var valueCnt = [];
+
+var WIDTH = 0;
+var HEIGHT = 0;
+
 function visualize(stream) {
     WIDTH = canvas.width;
     HEIGHT = canvas.height;
-    
+
     analyser.fftSize = 2048;
+    analyser.smoothingTimeConstant = 0.8;
+
     var bufferLength = analyser.frequencyBinCount; // half the FFT value
     var dataArray = new Uint8Array(bufferLength); // create an array to store the data
-
+    
     canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
     function draw() {
+        requestAnimationFrame(draw);
 
-        drawVisual = requestAnimationFrame(draw);
+        analyser.getByteFrequencyData(dataArray); // get waveform data and put it into the array created above
 
-        analyser.getByteTimeDomainData(dataArray); // get waveform data and put it into the array created above
+        if(smaplingCnt > samplingDuration) {
+            canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
-        canvasCtx.fillStyle = 'rgb(200, 200, 200)'; // draw wave with canvas
-        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-        canvasCtx.lineWidth = 2;
-        canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-
-        canvasCtx.beginPath();
-
-        var sliceWidth = WIDTH * 1.0 / bufferLength;
-        var x = 0;
-
-        for (var i = 0; i < bufferLength; i++) {
-
-            var v = dataArray[i] / 128.0;
-            var y = v * HEIGHT / 2;
-
-            if (i === 0) {
-                canvasCtx.moveTo(x, y);
-            } else {
-                canvasCtx.lineTo(x, y);
+            for (var i = 0; i < analyser.frequencyBinCount; i++) {
+                var value = valueCnt[i];
+                var percent = value / (256 * samplingDuration); // 256;
+                var height = HEIGHT * percent;
+                var offset = HEIGHT - height - 1;
+                var barWidth = WIDTH / analyser.frequencyBinCount;
+                var hue = i / analyser.frequencyBinCount * 360;
+                canvasCtx.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+                canvasCtx.fillRect(i * barWidth, offset, barWidth, height);        
             }
 
-            x += sliceWidth;
+            checkVowel(valueCnt);
+
+            smaplingCnt = 0;
+            valueCnt.length = 0;
+            valueCnt = [];
+            for(var j = 0; j < dataArray.length; j++) {
+                valueCnt[j] = 0;
+            }
+        } else { 
+            for (var i = 0; i < analyser.frequencyBinCount; i++) {
+                valueCnt[i] += dataArray[i];
+            }
         }
 
-        canvasCtx.lineTo(canvas.width, canvas.height / 2);
-        canvasCtx.stroke();
-
-    draw();
-
+        smaplingCnt = smaplingCnt + 1;
     }
 
+    draw();
+}
+
+// if you want something more accurate way then edit here!
+var btnA, btnI, btnU;
+
+function checkVowel(values) {
+    let chkA = false;
+    let chkI = false;
+    let chkU = false;
+    var sum = 0;
+    // A
+    var sum = 0;
+    for(var i = 256; i < 512; i++) {
+        sum += values[i];
+    }
+    if(sum > 2048) chkA = true;
+    
+    // I 
+    var sum = 0;
+    for(var i = 128; i < 256; i++) {
+        sum += values[i];
+    }
+    if(sum > 2048) chkI = true;
+
+    // U
+    var sum = 0;
+    for(var i = 0; i < 96; i++) {
+        sum += values[i];
+    }
+    for(var i = 96; i < 256; i++) {
+        sum -= values[i];
+    }
+    if(sum > 512) chkU = true;
+
+    // result
+    if(chkA) {
+        btnA.classList.add('active');        
+    } else {
+        btnA.classList.remove('active');
+    }
+    if(chkI) {
+        btnI.classList.add('active');        
+    } else {
+        btnI.classList.remove('active');
+    }
+    if(chkU) {
+        btnU.classList.add('active');        
+    } else {
+        btnU.classList.remove('active');
+    }
 }
